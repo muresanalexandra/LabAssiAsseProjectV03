b846c13b87d12e8e63ef5015985c8551a70d0c2e
Alexandra <muresan_alexandra2007@yahoo.com>
2019-03-23 14:10:50 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import Exceptions.*;
+import Repository.XMLFileRepository.NotaXMLRepo;
+import Repository.XMLFileRepository.StudentXMLRepo;
+import Repository.XMLFileRepository.TemaLabXMLRepo;
+import Service.XMLFileService.NotaXMLService;
+import Service.XMLFileService.StudentXMLService;
+import Service.XMLFileService.TemaLabXMLService;
+import Validator.*;
+import UI.*;
+import java.io.IOException;
+
+import static com.sun.deploy.trace.TraceLevel.UI;
+
+public class Main {
+    public static void main(String[] args) throws IOException, Exceptions.ValidatorException {
+        //System.out.println("Hello World!");
+        StudentValidator vs=new StudentValidator();
+        TemaLabValidator vt=new TemaLabValidator();
+        NotaValidator vn=new Validator.NotaValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+        StudentXMLService stsrv=new StudentXMLService(strepo);
+        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+        ui ui=new ui(stsrv,tmsrv,ntsrv);
+        ui.run();
+    }
+}
